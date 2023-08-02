a6a03b8707c7590fb6dbece7a05793df61315884
mor meir <123agmor@gmail.com>
2023-08-02 18:34:17 +0300

    select c/f

---
+++ src/assets/consts.js
@@ -0,0 +1,4 @@
+export const WEATHER_TYPES = [
+    {value: 'celsius', title: 'Celsius'}, 
+    {value: 'fahrenheit', title: 'Fahrenheit'}
+]
---
+++ src/components/common/SelectField.jsx
@@ -0,0 +1,34 @@
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+import PropTypes from 'prop-types'
+
+export default function SelectField({options = [], defaultValue, select = ()=>{}}) {
+  const handleChange = (e) => {
+    select(e.target.value)
+  }
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <NativeSelect
+          defaultValue={defaultValue}
+          inputProps={{
+            name: 'age',
+            id: 'uncontrolled-native',
+          }}
+          onChange={handleChange}
+        >
+            {options.length && options.map(option => {
+                return <option key={option.value} value={option.value}>{option.title}</option>
+            })}
+        </NativeSelect>
+      </FormControl>
+    </Box>
+  );
+}
+
+SelectField.propTypes = {
+  select: PropTypes.func,
+  options: PropTypes.array,
+  defaultValue: PropTypes.string || PropTypes.number
+}
